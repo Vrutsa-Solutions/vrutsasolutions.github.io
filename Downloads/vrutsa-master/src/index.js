a26d6377d7b0b471647214063baca6a23f1a6716
MayankVaswani16 <mayankvaswani81@gmail.com>
2026-05-19 09:27:47 +0530

    base

---
+++ Downloads/vrutsa-master/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Website from "./website";
+import "./website.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <Website />
+  </React.StrictMode>,
+);
